86e5f50c1eb710b086343983b662114faa76c75a
Paulo Alexandre <pacalexandre@gmail.com>
2022-07-11 07:43:09 -0300

    ajustes

---
+++ imdb/src/test/java/com/api/imdb/ImdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.imdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
