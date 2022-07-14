fcca93cb092c79673a5afc7b827561480bae3c2f
Paulo Alexandre <pacalexandre@gmail.com>
2022-07-14 01:58:32 +0000

    projeto java springo rodando

---
+++ src/main/java/com/service/apiimdbjava/ApiImdbJavaApplication.java
@@ -0,0 +1,13 @@
+package com.service.apiimdbjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiImdbJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiImdbJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/apiimdbjava/ApiImdbJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.apiimdbjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiImdbJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
